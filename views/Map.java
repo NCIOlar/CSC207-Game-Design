16e68895ec46a007502310fc20687356d56cfd43
Alan Wang <alanw1588@gmail.com>
2023-11-24 22:40:49 -0500

    Fixed tunnels not showing

--- views/Map.java
+++ views/Map.java
@@ -49,28 +49,35 @@ public class Map {
                     GridPane.setColumnIndex(blackBox, j);
                     map.getChildren().add(blackBox);
                 }else if (room.equals("|")) {
-                    String passageImage = game.getDirectoryName() + "/room-images/passage.png";
-                    Image passageImageFile = new Image(passageImage);
-                    ImageView passage = new ImageView(passageImageFile);
-                    passage.setFitHeight(100);
-                    passage.setFitWidth(100);
-                    GridPane.setRowIndex(passage, i);
-                    GridPane.setColumnIndex(passage, j);
-                    map.getChildren().add(passage);
+                    if(checkSurroundings(i, j)){
+                        String passageImage = game.getDirectoryName() + "/room-images/passage.png";
+                        Image passageImageFile = new Image(passageImage);
+                        ImageView passage = new ImageView(passageImageFile);
+                        passage.setFitHeight(100);
+                        passage.setFitWidth(100);
+                        GridPane.setRowIndex(passage, i);
+                        GridPane.setColumnIndex(passage, j);
+                        map.getChildren().add(passage);
+                    }else{
+                        Rectangle blackBox = new Rectangle(100, 100, Color.BLACK);
+                        GridPane.setRowIndex(blackBox, i);
+                        GridPane.setColumnIndex(blackBox, j);
+                        map.getChildren().add(blackBox);
+                    }
                 } else {
-//                    if(rooms.get(Integer.parseInt(room)).getVisited()) {
+                    if(rooms.get(Integer.parseInt(room)).getVisited()) {
                         ImageView roomImage = getImage(Integer.parseInt(room));
                         roomImage.setFitHeight(100);
                         roomImage.setFitWidth(100);
                         GridPane.setRowIndex(roomImage, i);
                         GridPane.setColumnIndex(roomImage, j);
                         map.getChildren().add(roomImage);
-//                    }else{
-//                        Rectangle blackBox = new Rectangle(100, 100, Color.BLACK);
-//                        GridPane.setRowIndex(blackBox, i);
-//                        GridPane.setColumnIndex(blackBox, j);
-//                        map.getChildren().add(blackBox);
-//                    }
+                    }else{
+                        Rectangle blackBox = new Rectangle(100, 100, Color.BLACK);
+                        GridPane.setRowIndex(blackBox, i);
+                        GridPane.setColumnIndex(blackBox, j);
+                        map.getChildren().add(blackBox);
+                    }
                 }
             }
         }
@@ -92,12 +99,34 @@ public class Map {
         return new ImageView(roomImageFile);
     }
 
+    private boolean checkSurroundings(int row, int column){
+        try{
+            int top = Integer.parseInt(blueprint.get(row + 1)[column]);
+            return rooms.get(top).getVisited();
+        }catch(Exception e){
+            try{
+                int bot = Integer.parseInt(blueprint.get(row - 1)[column]);
+                return rooms.get(bot).getVisited();
+            }catch(Exception f){
+                try{
+                    int left = Integer.parseInt(blueprint.get(row)[column - 1]);
+                    return rooms.get(left).getVisited();
+
+                }catch(Exception g){
+                    try{
+                        int right = Integer.parseInt(blueprint.get(row - 1)[column + 1]);
+                        return rooms.get(right).getVisited();
+                    }catch(Exception h){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
     public Pane showMap(){
 
         return map;
     }
 
-    public void hideMap(){
-
-    }
 }
